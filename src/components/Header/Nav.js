fe1c206261e10da8f05e6bcb2d8819b1b33f12b7
Kenny Elshoff <kelshoff@grahamdigital.com>
2020-05-09 22:19:49 -0400

    Use gatsby-image for header logo

--- src/components/Header/Nav.js
+++ src/components/Header/Nav.js
@@ -1,14 +1,31 @@
 /** @jsx jsx */
 import { useState } from "react";
 import PropTypes from "prop-types";
-import { jsx } from "theme-ui";
+import Img from "gatsby-image";
+import { useStaticQuery, graphql } from "gatsby";
+import { Box, Container, jsx } from "theme-ui";
 import { Flex, Inline } from "raam";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 import StyledLink from "../Link.styled";
-import logo from "../../images/logo.png";
 
 const Nav = ({ navLinks }) => {
+  const {
+    file: {
+      childImageSharp: { fluid },
+    },
+  } = useStaticQuery(graphql`
+    query LogoQuery {
+      file(relativePath: { eq: "logo.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 200, maxHeight: 153) {
+            ...GatsbyImageSharpFluid_withWebp_noBase64
+          }
+        }
+      }
+    }
+  `);
+
   const [menuOpenState, setMenuOpenState] = useState(false);
 
   const burgerHandler = () => {
@@ -16,39 +33,44 @@ const Nav = ({ navLinks }) => {
   };
 
   return (
-    <Flex
-      alignItems="center"
-      bg="base.dark"
-      className="nav"
-      justifyContent="space-between"
-      px={["3", null, null, null, "0"]}
-      py="2"
-      sx={{ height: "24" }}
-    >
-      <img alt="Charleston Fun Park" height="153" src={logo} width="200" />
-      <Inline gap="5">
-        {navLinks.map((link) => (
-          <StyledLink
-            key={link.name}
-            color="black"
-            fontSize="2"
-            fontWeight="bold"
-            href={link.url}
-            letterSpacing="wide"
-            textDecoration="none"
-            textTransform="uppercase"
-          >
-            {link.name}
-          </StyledLink>
-        ))}
-        <FontAwesomeIcon
-          icon={menuOpenState ? faTimes : faBars}
-          sx={{ cursor: "pointer", fontSize: "5" }}
-          fixedWidth
-          onClick={burgerHandler}
-        />
-      </Inline>
-    </Flex>
+    <Box bg="base.dark" px={["3", null, null, null, "0"]}>
+      <Container>
+        <Flex
+          alignItems="center"
+          className="nav"
+          justifyContent="space-between"
+          sx={{ height: "24" }}
+        >
+          <Img
+            alt="Charleston Fun Park"
+            fluid={fluid}
+            sx={{ width: "200px", height: "153px" }}
+          />
+          <Inline gap="5">
+            {navLinks.map((link) => (
+              <StyledLink
+                key={link.name}
+                color="black"
+                fontSize="2"
+                fontWeight="bold"
+                href={link.url}
+                letterSpacing="wide"
+                textDecoration="none"
+                textTransform="uppercase"
+              >
+                {link.name}
+              </StyledLink>
+            ))}
+            <FontAwesomeIcon
+              icon={menuOpenState ? faTimes : faBars}
+              sx={{ cursor: "pointer", fontSize: "5" }}
+              fixedWidth
+              onClick={burgerHandler}
+            />
+          </Inline>
+        </Flex>
+      </Container>
+    </Box>
   );
 };
 
