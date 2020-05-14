2b00192d83b63401016b106a50e634ab99c65ef7
Kenny Elshoff <1545970+ReallyBadNews@users.noreply.github.com>
2020-05-14 01:20:17 -0400

    Start of attractions section, fixed Arrow width prop

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,14 @@ import MainLayout from "../templates/MainLayout";
 import Hero from "../components/Hero";
 import SectionTitle from "../components/SectionDivider/SectionTitle";
 import BirthdaySection from "../components/BirthdaySection";
+import AttractionsSection from "../components/AttractionsSection";
 
 const IndexPage = () => (
   <MainLayout title="Home">
     <Hero />
     <SectionTitle />
     <BirthdaySection />
+    <AttractionsSection />
   </MainLayout>
 );
 
