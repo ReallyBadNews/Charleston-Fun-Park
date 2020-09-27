474a772d226c1fc2e51f1a745f3268c21801b4da
Kenny Elshoff <kelshoff@grahamdigital.com>
2020-09-27 13:46:53 -0400

    Center hero typography

--- src/components/HomeSections/Hero.js
+++ src/components/HomeSections/Hero.js
@@ -104,7 +104,7 @@ const Hero = () => {
           background: [
             `none`,
             null,
-            `linear-gradient(90deg, #1D2435FF, #1D2435FF 50%, #1D243500 100%)`,
+            `radial-gradient(circle, #1D2435FF, 50%, #1D243500 100%)`,
           ],
           opacity: `0.5`,
         }}
@@ -122,8 +122,10 @@ const Hero = () => {
           >
             <Box
               color="white.light"
+              mx="auto"
               sx={{
-                width: [null, null, `3/4`, null, `7/12`],
+                width: `3/4`,
+                textAlign: `center`,
                 textShadow: `text`,
               }}
             >
