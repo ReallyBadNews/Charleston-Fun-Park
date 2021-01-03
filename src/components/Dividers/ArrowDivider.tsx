5f2af0e14bd8bbcf5162f0531e695deaa60c554e
Kenny Elshoff <kelshoff@grahamdigital.com>
2021-01-03 09:05:44 -0500

    Remove unused import

--- src/components/Dividers/ArrowDivider.tsx
+++ src/components/Dividers/ArrowDivider.tsx
@@ -1,7 +1,6 @@
 /** @jsx jsx */
 
 import { Container, Divider, Text, jsx } from "theme-ui";
-import { useBreakpoint } from "gatsby-plugin-breakpoints";
 import { Inline } from "raam";
 import Arrow from "@/components/Images/Arrow";
 import BrickBg from "@/components/Images/BrickBg";
@@ -12,48 +11,41 @@ interface ArrowDividerProps {
   id?: string;
 }
 
-const ArrowDivider: FC<ArrowDividerProps> = ({ id }) => {
-  const breakpoints = useBreakpoint();
-
-  return (
-    <BrickBg id={id}>
-      <Divider sx={{ position: "absolute", top: "0" }} variant="divider.glow" />
-      <Container>
-        <Inline
-          alignItems="center"
-          gap={["2", null, "3", null, "4"]}
-          justifyContent="center"
-          py="3"
-        >
-          <Arrow
-            sx={{
-              height: "auto",
-              transform: "rotateZ(180deg)",
-              width: ["72px", null, "24", null, "32"],
-            }}
-          />
-          <StyledLink scrollTo="birthdays">
-            <Text
-              variant="display"
-              sx={{ maxWidth: ["14ch", null, "initial"] }}
-            >
-              The Party Starts Here
-            </Text>
-          </StyledLink>
-          <Arrow
-            sx={{
-              height: "auto",
-              width: ["72px", null, "24", null, "32"],
-            }}
-          />
-        </Inline>
-      </Container>
-      <Divider
-        sx={{ position: "absolute", bottom: "0" }}
-        variant="divider.glow"
-      />
-    </BrickBg>
-  );
-};
+const ArrowDivider: FC<ArrowDividerProps> = ({ id }) => (
+  <BrickBg id={id}>
+    <Divider sx={{ position: "absolute", top: "0" }} variant="divider.glow" />
+    <Container>
+      <Inline
+        alignItems="center"
+        gap={["2", null, "3", null, "4"]}
+        justifyContent="center"
+        py="3"
+      >
+        <Arrow
+          sx={{
+            height: "auto",
+            transform: "rotateZ(180deg)",
+            width: ["72px", null, "24", null, "32"],
+          }}
+        />
+        <StyledLink scrollTo="birthdays">
+          <Text variant="display" sx={{ maxWidth: ["14ch", null, "initial"] }}>
+            The Party Starts Here
+          </Text>
+        </StyledLink>
+        <Arrow
+          sx={{
+            height: "auto",
+            width: ["72px", null, "24", null, "32"],
+          }}
+        />
+      </Inline>
+    </Container>
+    <Divider
+      sx={{ position: "absolute", bottom: "0" }}
+      variant="divider.glow"
+    />
+  </BrickBg>
+);
 
 export default ArrowDivider;
