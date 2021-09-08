340cb724387a694ae5a5f7de5679b5b341a8d6c2
Kenny Elshoff <kelshoff@grahamdigital.com>
2021-09-08 00:16:29 -0400

    fix: adjust tracking pixel position
    Signed-off-by: Kenny Elshoff <kelshoff@grahamdigital.com>

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,9 +6,11 @@ export const wrapRootElement = ({ element }) => (
       src="https://tags.w55c.net/rs?id=0f94f5ee47224e9085965cb413903a1d&t=marketing"
       style={{
         position: "absolute",
-        top: "0",
+        top: "50%",
+        transform: "translateY(-50%)",
         left: "0",
         zIndex: "-1000",
+        textIndent: "-9999px",
       }}
       aria-hidden
     />
@@ -18,9 +20,11 @@ export const wrapRootElement = ({ element }) => (
       src="https://secure.adnxs.com/seg?add=24451561&t=2"
       style={{
         position: "absolute",
-        top: "0",
+        top: "50%",
+        transform: "translateY(-50%)",
         left: "0",
         zIndex: "-1000",
+        textIndent: "-9999px",
       }}
       width="1"
       aria-hidden
